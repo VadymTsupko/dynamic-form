a50d35f64a18f99f558c1104fd18f3624106380c
vadymt <>
2025-07-05 20:24:13 +0300

    feat: connect msw to project

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,42 @@ import { Provider } from "react-redux"
 import { App } from "./App"
 import { store } from "./app/store"
 
-const container = document.getElementById("root")
+async function prepareWorker() {
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    const { worker } = await import("../mocks/worker")
+    return worker.start({
+      serviceWorker: {
+        url: "/mockServiceWorker.js",
+      },
+    })
+  }
 
-if (container) {
-  const root = createRoot(container)
-
-  root.render(
-    <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </StrictMode>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
+  return Promise.resolve()
 }
+
+prepareWorker()
+  .then(() => {
+    const container = document.getElementById("root")
+
+    if (container) {
+      const root = createRoot(container)
+
+      root.render(
+        <StrictMode>
+          <Provider store={store}>
+            <App />
+          </Provider>
+        </StrictMode>,
+      )
+    } else {
+      throw new Error(
+        "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+      )
+    }
+  })
+  .catch((reason: unknown) => {
+    console.error("Worker error", reason)
+  })
